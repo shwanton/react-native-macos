ff61b1010e4b733976f94824a87501d94e94fdc5
Anandraj Govindan <anandrag@microsoft.com>
2022-09-29 16:04:37 -0700

    Reverting the old logic in engine selection to old behaviour

--- android-patches/patches/V8/ReactAndroid/src/main/java/com/facebook/react/ReactInstanceManagerBuilder.java
+++ android-patches/patches/V8/ReactAndroid/src/main/java/com/facebook/react/ReactInstanceManagerBuilder.java
@@ -1,5 +1,5 @@
---- "C:\\github\\react-native\\ReactAndroid\\src\\main\\java\\com\\facebook\\react\\ReactInstanceManagerBuilder.java"	2022-08-05 12:59:58.286964700 +0530
-+++ "C:\\github\\react-native-macos\\ReactAndroid\\src\\main\\java\\com\\facebook\\react\\ReactInstanceManagerBuilder.java"	2022-08-05 15:08:37.009898300 +0530
+--- "\\ReactAndroid\\src\\main\\java\\com\\facebook\\react\\ReactInstanceManagerBuilder.java"	2022-09-29 15:47:40.515043600 -0700
++++ "\\ReactAndroid\\src\\main\\java\\com\\facebook\\react\\ReactInstanceManagerBuilder.java"	2022-09-29 15:57:50.948475300 -0700
 @@ -34,6 +34,8 @@
  import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler;
  import com.facebook.react.packagerconnection.RequestHandler;
@@ -9,13 +9,32 @@
  import java.util.ArrayList;
  import java.util.List;
  import java.util.Map;
-@@ -397,6 +399,9 @@
+@@ -382,22 +384,15 @@
+     // if nothing is specified, use old loading method
+     // else load the required engine
+     if (jsInterpreter == JSInterpreter.OLD_LOGIC) {
+-      try {
+-        // If JSC is included, use it as normal
+-        initializeSoLoaderIfNecessary(applicationContext);
+-        JSCExecutor.loadLibrary();
+-        return new JSCExecutorFactory(appName, deviceName);
+-      } catch (UnsatisfiedLinkError jscE) {
+-        if (jscE.getMessage().contains("__cxa_bad_typeid")) {
+-          throw jscE;
+-        }
+-        HermesExecutor.loadLibrary();
+-        return new HermesExecutorFactory();
+-      }
++      V8Executor.loadLibrary();
++      return new V8ExecutorFactory(appName, deviceName);
      } else if (jsInterpreter == JSInterpreter.HERMES) {
        HermesExecutor.loadLibrary();
        return new HermesExecutorFactory();
+-    } else {
 +    } else if(jsInterpreter == JSInterpreter.V8) {
 +      V8Executor.loadLibrary();
 +      return new V8ExecutorFactory(appName, deviceName);
-     } else {
++     } else {
        JSCExecutor.loadLibrary();
        return new JSCExecutorFactory(appName, deviceName);
+     }
