0e529f0a100c366c6624d430d73d5149bb3dea37
Shawn Dempsey <shawndempsey@fb.com>
2023-06-16 08:48:52 -0700

    MacOS uses same AppDelegate as RN Core HelloWorld template

--- local-cli/generator-macos/templates/macos/HelloWorld-macOS/AppDelegate.m
+++ local-cli/generator-macos/templates/macos/HelloWorld-macOS/AppDelegate.m
@@ -10,15 +10,9 @@
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
-  
+
   return [super applicationDidFinishLaunching:notification];
 }
-
-- (void)applicationWillTerminate:(NSNotification *)aNotification {
-  // Insert code here to tear down your application
-}
-
-#pragma mark - RCTBridgeDelegate Methods
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
@@ -26,20 +20,6 @@
   return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
 #else
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
-#endif
-}
-
-/// This method controls whether the `concurrentRoot`feature of React18 is turned on or off.
-///
-/// @see: https://reactjs.org/blog/2022/03/29/react-v18.html
-/// @note: This requires to be rendering on Fabric (i.e. on the New Architecture).
-/// @return: `true` if the `concurrentRoot` feature is enabled. Otherwise, it returns `false`.
-- (BOOL)concurrentRootEnabled
-{
-#ifdef RN_FABRIC_ENABLED
-  return true;
-#else
-  return false;
 #endif
 }
 
