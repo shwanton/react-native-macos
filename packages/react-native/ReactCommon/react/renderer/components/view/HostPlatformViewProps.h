3ef4f80b68634bbcea38cc628aecd39ca6b7b4a1
Nick Lefever <lefever@meta.com>
2024-01-29 20:11:12 +0100

    [fabric] Add HostPlatformViewProps to ViewProps
    Summary: This diff updates `ViewProps` with a reference to the `HostPlatformViewProps`, allowing to move macOS specific properties in a separate class without having to change `ViewProps`.
    
    Test Plan: Tested later in this stack.
    
    Reviewers: shawndempsey, #rn-desktop
    
    Reviewed By: shawndempsey
    
    Differential Revision: https://phabricator.intern.facebook.com/D53241732
    
    Tasks: T157889406, T154618477

---
+++ packages/react-native/ReactCommon/react/renderer/components/view/HostPlatformViewProps.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include <react/renderer/core/Props.h>
+#include <react/renderer/core/PropsParserContext.h>
+
+namespace facebook::react {
+class HostPlatformViewProps {
+ public:
+  HostPlatformViewProps() = default;
+  HostPlatformViewProps(
+      const PropsParserContext &context,
+      const HostPlatformViewProps &sourceProps,
+      const RawProps &rawProps,
+      bool shouldSetRawProps = true) {}
+
+  void
+  setProp(
+      const PropsParserContext &context,
+      RawPropsPropNameHash hash,
+      const char *propName,
+      RawValue const &value) {}
+};
+} // namespace facebook::react
