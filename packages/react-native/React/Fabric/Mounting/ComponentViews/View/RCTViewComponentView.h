7ecb2f6fe1b2fd8c80a9019b4b4c2c7004e9df0a
Nick Lefever <lefever@meta.com>
2024-02-13 15:39:36 +0100

    [fabric] Fix missing method declaration of View component
    Summary: See title.
    
    Test Plan: Sandcastle build D53695263
    
    Reviewers: shawndempsey, #rn-desktop
    
    Reviewed By: shawndempsey
    
    Differential Revision: https://phabricator.intern.facebook.com/D53711568

--- packages/react-native/React/Fabric/Mounting/ComponentViews/View/RCTViewComponentView.h
+++ packages/react-native/React/Fabric/Mounting/ComponentViews/View/RCTViewComponentView.h
@@ -77,6 +77,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 #if TARGET_OS_OSX // [macOS
 - (BOOL)handleKeyboardEvent:(NSEvent *)event;
+- (void)buildDataTransferItems:(std::vector<DataTransferItem> &)dataTransferItems forPasteboard:(NSPasteboard *)pasteboard;
 #endif // macOS]
 
 /*
